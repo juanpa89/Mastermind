94c91ed216630e93b5579223e596ab9569c0890c
Juan Pablo Rojas <juanpa89@gmail.com>
2020-11-05 16:27:42 -0300

    Gives feedback on the user input

--- script.rb
+++ script.rb
@@ -20,7 +20,7 @@ class Line
     end
 
     def get_answer
-        puts "\n\nChoose your numbers"
+        puts "\nChoose your numbers"
         answer = gets.chomp
         while answer[0].to_i > 6 || answer[1].to_i > 6 || answer[2].to_i > 6 || answer[3].to_i > 6
             puts "\n\nChoose your numbers, they have to be numbers from 0 to 6."
@@ -34,7 +34,37 @@ class Line
         
     
     def give_feedback
+        position_list = [self.p1, self.p2, self.p3, self.p4]
+        ordered_answer = ['-','-','-','-']
+        #check if is in the position and give \u2022
+        index_1 = 0
+        for i in position_list
+            x = index_1
+            for j in $random_numbers
+                if i == j
+                    ordered_answer[x] = ("*")
+                    break
+                end
+            end
+            index_1 += 1
+        end
+        
+        index = 0
+        while index < 4 do 
+            i = index
+            if position_list[i] == $random_numbers[i]
+                ordered_answer[i] = ("\u2022")
+            end
+            index += 1
+        end
 
+        #make the answer to be in random place
+        ordered_answer.shuffle!
+
+        self.a1 = ordered_answer[0]
+        self.a2 = ordered_answer[1]
+        self.a3 = ordered_answer[2]
+        self.a4 = ordered_answer[3]
     end
 
 
@@ -48,7 +78,7 @@ class Line
         elsif self.number == 2
             "\t#{self.number}  |  #{self.p1}#{self.p2}#{self.p3}#{self.p4}  |  |#{self.a1}#{self.a2}#{self.a3}#{self.a4}\t \u2022 = 100% Correct."
         elsif self.number == 1
-            "\t#{self.number}  |  #{self.p1}#{self.p2}#{self.p3}#{self.p4}  |  |#{self.a1}#{self.a2}#{self.a3}#{self.a4}\t * = Right Number, Wrong Space."
+            "\t#{self.number}  |  #{self.p1}#{self.p2}#{self.p3}#{self.p4}  |  |#{self.a1}#{self.a2}#{self.a3}#{self.a4}\t * = Right Number, Wrong Space.\n\n"
         else
             "\t#{self.number}  |  #{self.p1}#{self.p2}#{self.p3}#{self.p4}  |  |#{self.a1}#{self.a2}#{self.a3}#{self.a4}"
         end
@@ -63,7 +93,7 @@ class Board
     attr_accessor :l1, :l2, :l3, :l4, :l5, :l6, :l7, :l8, :l9,
      :l10, :l11, :l12, :board, :random_numbers, :show_numbers
     def initialize
-        @random_numbers = []
+        
         @show_numbers = "    "
 
         @l1 = Line.new(1)
@@ -79,9 +109,10 @@ class Board
         @l11 = Line.new(11)
         @l12 = Line.new(12)
     end
-
+=begin self.show_numbers 
+=end
     def print_board
-        puts "\n\n\t\t ->#{self.show_numbers}<-
+        puts "\n\n\t\t ->#{$random_numbers}<-
         #{self.l12.make_line}
         #{self.l11.make_line}
         #{self.l10.make_line}
@@ -96,21 +127,24 @@ class Board
         #{self.l1.make_line}"
     end
 
-    def select_number
-        4.times do
-            self.random_numbers.push(rand(1..6))
-        end
+    
+end
+
+def select_numbers
+    4.times do
+        $random_numbers.push(rand(1..6).to_s)
     end
 end
 
+$random_numbers = []
 
 set_title
 board = Board.new
-board.select_number
+select_numbers
 board.print_board
 board.l1.get_answer
 system("clear")
 set_title
 board.print_board
-
-
+board.l1.give_feedback
+board.print_board
